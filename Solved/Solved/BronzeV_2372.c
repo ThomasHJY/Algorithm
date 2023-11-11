0f70953f2a12278cb8722447fbfaa05a232c8ceb
Thomas <jythomas@naver.com>
2023-11-11 20:37:05 +0900

    완료

---
+++ Solved/Solved/BronzeV_2372.c
@@ -0,0 +1,28 @@
+/*
+with Ada.Text_IO; use Ada.Text_IO;
+procedure Table is
+begin
+	Put_Line("Animal      Count");
+	Put_Line("-----------------");
+	Put_Line("Chickens      100");
+	Put_Line("Clydesdales     5");
+	Put_Line("Cows           40");
+	Put_Line("Goats          22");
+	Put_Line("Steers          2");
+end Table;
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Animal      Count\n");
+	printf("-----------------\n");
+	printf("Chickens      100\n");
+	printf("Clydesdales     5\n");
+	printf("Cows           40\n");
+	printf("Goats          22\n");
+	printf("Steers          2\n");
+
+	return 0;
+}

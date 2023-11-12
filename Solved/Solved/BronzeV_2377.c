12d77e13d53ccd17d459c16a9bfeb1f803e04fb4
Thomas <jythomas@naver.com>
2023-11-12 23:20:45 +0900

    완료

---
+++ Solved/Solved/BronzeV_2377.c
@@ -0,0 +1,32 @@
+/*
+PRINT " _________"
+PRINT " \_     _/"
+PRINT "   \   /"
+PRINT "    | |"
+PRINT "   /   \"
+PRINT "  /     \"
+PRINT " |       |"
+PRINT "/---------\"
+PRINT "| \-/ \-/ |"
+PRINT "\---------/"
+PRINT " \_______/"
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf(" _________\n");
+	printf(" \\_     _/\n");
+	printf("   \\   /\n");
+	printf("    | |\n");
+	printf("   /   \\\n");
+	printf("  /     \\\n");
+	printf(" |       |\n");
+	printf("/---------\\\n");
+	printf("| \\-/ \\-/ |\n");
+	printf("\\---------/\n");
+	printf(" \\_______/\n");
+
+	return 0;
+}
